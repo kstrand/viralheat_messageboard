db983207207673eb251200ea333c597437ec7304
Keith <keith.strand@gmail.com>
2013-01-21 15:22:59 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VhMessageboard::Application.config.secret_token = 'd6f5e0e232b1dc08072629eb726a91a2a8fe518210b3931db3c6e6ba835c706ad8bcc358b0609f9921bdbeb8caf023c1b26d326a162254cd3bbbcc30b659a97c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+VhMessageboard::Application.config.session_store :cookie_store, key: '_vh_messageboard_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# VhMessageboard::Application.config.session_store :active_record_store
