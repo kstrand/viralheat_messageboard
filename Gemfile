5f679daaac3e43c527147ef87100e0389973d1d7
Keith <keith.strand@gmail.com>
2013-01-21 16:47:37 -0800

    add Devise gem for authentication learn VIM directory navigation

--- Gemfile
+++ Gemfile
@@ -3,8 +3,8 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.11'
 
 # Bundle edge Rails instead:
-# gem 'rails', :git => 'git://github.com/rails/rails.git'
-
+# gem 'rails', :git => 'git://github.com/rails/rails.git
+gem 'devise'
 gem 'sqlite3'
 
 
--- config/application.rb
+++ config/application.rb
@@ -59,8 +59,11 @@ module VhMessageboard
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
+
     # Enable the asset pipeline
     config.assets.enabled = true
+    # needs to be false for heroku deployment
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
